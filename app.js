a1c0133f170455821c3300ba5a540939f649a7f5
Ryan Oh <ohryan55@gmail.com>
2020-07-02 21:04:32 -0700

    Setting up express backend and connection to MongoDB

---
+++ app.js
@@ -0,0 +1,69 @@
+let createError = require('http-errors');
+let express = require('express');
+let cors = require('cors');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+require('dotenv').config()
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+// ---------- MongoDB connection ----------
+
+const MongoClient = require('mongodb').MongoClient
+
+let uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + process.env.DB_ADDR + process.env.DB_NAME + "?retryWrites=true&w=majority";
+
+MongoClient.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true })
+  .then(client => {
+    let db = client.db(process.env.DB_NAME);
+    app.locals.db = db;
+
+    // SET UP COLLECTION REFERENCES HERE
+    app.locals.messages = db.collection('messages');
+
+    console.log("Connected to MongoDB!");
+  }).catch(error => {
+  console.error(error);
+});
+
+// ----------------------------------
+
+
+module.exports = app;
