c40cdc5324593e8d181e75894865ec59fb6b2df5
Ryan Oh <ohryan55@gmail.com>
2020-07-06 19:03:21 -0700

    Get by userif

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const ObjectID = require("mongodb").ObjectID;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,9 +14,25 @@ router.get('/', function(req, res, next) {
     });
 });
 
+// Get a user by id
+router.get('/:id', function(req, res, next) {
+  let id = ObjectID(req.params.id);
+
+  req.app.locals.users.findOne({
+    _id: id
+  })
+    .then(result => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(result);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+
 // Registers new user
 // API request should have 'username' and 'email' in body
-
 router.post('/', function(req, res, next) {
   const newUser = {
     username: req.body.username,
