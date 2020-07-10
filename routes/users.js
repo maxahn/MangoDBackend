07018926aed2f8fd2b63012aaaf3d736d4e1f9dd
AamirL1011 <aamirs@students.cs.ubc.ca>
2020-07-09 21:32:37 -0700

    updated routes

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const ObjectID = require("mongodb").ObjectID;
 const multer  = require('multer');
 const upload = multer({ dest: 'uploads/' });
-const Binary = require("mongodb").Binary;
-//const fs = require('fs');
 
 
 /* GET users listing. */
@@ -83,9 +81,9 @@ router.post('/', function(req, res, next) {
 });
 
 /* PUT user: add clap to user  */
-router.put('/feed/claps/:task_id', (req, res, next) => {
-  const { value, user_id } = req.body;
-  const userID = ObjectID(user_id);
+router.put('/feed/claps/:user_id', (req, res, next) => {
+  const { value } = req.body;
+  const userID = ObjectID(req.params.user_id);
   req.app.locals.users.updateOne(
     { _id: userID },
     {
@@ -101,9 +99,9 @@ router.put('/feed/claps/:task_id', (req, res, next) => {
 });
 
 /* PUT user: add mango to user  */
-router.put('/feed/mangos/:task_id', (req, res, next) => {
-  const { numMango, user_id } = req.body;
-  const userID = ObjectID(user_id);
+router.put('/feed/mangos/:user_id', (req, res, next) => {
+  const { numMango } = req.body;
+  const userID = ObjectID(req.params.user_id);
   req.app.locals.users.updateOne(
     { _id: userID },
     {
@@ -121,16 +119,27 @@ router.put('/feed/mangos/:task_id', (req, res, next) => {
 router.put('/profile/avatar/:user_id', upload.single('image'), (req, res, next) => {
    const user_id = ObjectID(req.params.user_id);
    const { image } = req.body;
-   //const img = fd.get("image");
-   console.log(req.body);
-   //let bitmap = fs.readFileSync(image);
-   //let encImage = new Buffer(bitmap).toString('base64');
-   //const buf = Buffer.from(image).toString('base64');
-   //const bin = Binary(image, 0);
    req.app.locals.users.updateOne(
     { _id: user_id },
     {
       $set: { avatar: image }
+    }
+  ).then((result) => {
+    res.status(200).end();
+  }).catch(err => {
+    console.error(err);
+    res.status(503).end();
+  });
+});
+
+/* PUT user: update username of user  */
+router.put('/profile/name/:user_id', (req, res, next) => {
+  const { newName } = req.body;
+  const userID = ObjectID(req.params.user_id);
+  req.app.locals.users.updateOne(
+    { _id: userID },
+    {
+      $set: { username: newName }
     }
   ).then((result) => {
     res.status(200).end();
