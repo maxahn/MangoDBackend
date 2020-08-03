510bc55d0c9e5859023e177d0258340da3823b66
Ryan Oh <ohryan55@gmail.com>
2020-08-03 16:21:16 -0700

    more build

---
+++ build/precache-manifest.7564e867e482371a9af2d5d389423140.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6609f1f66835703730065b71e8e49723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c05c2a7bcf9cec146562",
+    "url": "/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "2624fc9ed43960acc5fa",
+    "url": "/static/css/main.4e19bd4e.chunk.css"
+  },
+  {
+    "revision": "c05c2a7bcf9cec146562",
+    "url": "/static/js/2.e26f1d6b.chunk.js"
+  },
+  {
+    "revision": "d50512915a9d93353b354eb67f2df637",
+    "url": "/static/js/2.e26f1d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2624fc9ed43960acc5fa",
+    "url": "/static/js/main.49d49259.chunk.js"
+  },
+  {
+    "revision": "c05652fe6ebaf1cdc26e",
+    "url": "/static/js/runtime-main.8d8fc197.js"
+  },
+  {
+    "revision": "42a9d815e23a4405cef4a9de201f97ce",
+    "url": "/static/media/mango-tree.42a9d815.svg"
+  }
+]);
